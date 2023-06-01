e0abec1f50d6da31681c16306e0dfe4f43946dac
whitneywishart <whitneywishart@gmail.com>
2023-06-01 10:50:26 -0700

    update gitignore

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const db = require('./config/connection.js');
 
 
 const app = express();
